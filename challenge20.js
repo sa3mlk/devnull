57f09e31dfcd45c503cdfebc4ff132a9212abc9f
OxanaSachenkova <oxana.sachenkova@duego.com>
2012-04-14 12:22:59 +0200

    Challenge 20 updated

--- challenge20.js
+++ challenge20.js
@@ -4,10 +4,10 @@ function addButtonsToNode(howmany, domnode)
 	{
 		var div = document.createElement("div");
 		domnode.appendChild(div);
-		div.innerHTML = i;
-		div.onClick = function(e)
+		div.innerHTML = i; 
+		div.onclick = function(e)
 		{
-			alert(i);
+			alert(div.innerHTML); // if alerting i, always will be the last number 
 		}
 	}
 }
